ff227a4ced07bf31ce2bfdc34b3dc264990398f9
Marcel Otto <marcelotto@gmx.de>
2024-07-10 20:16:41 +0200

    Add working cleanup in service test cases

--- test/support/service_case.ex
+++ test/support/service_case.ex
@@ -14,17 +14,29 @@ defmodule Ontogen.ServiceCase do
     end
   end
 
+  alias Ontogen.Operations.CleanCommand
+
   def clean_service!(context) do
     config_opts = (context[:config] || []) |> Keyword.put(:log, false)
     {setup?, boot_opts} = Keyword.pop(config_opts, :setup, true)
 
     start_supervised({Ontogen, boot_opts})
 
-    if Ontogen.status() == :ready do
-      Ontogen.clean_dataset!()
-    end
-
     if setup?, do: Ontogen.setup!()
+
+    if Map.get(context, :clean_dataset, true) do
+      # We can not use Ontogen.clean_dataset!() here because `on_exit` runs in a
+      # separate process after the test process has terminated. This means the
+      # Ontogen GenServer started above with `start_supervised` is no longer.
+      on_exit(fn ->
+        with {:ok, service} <- Ontogen.Config.service() do
+          case CleanCommand.call(service, :all) do
+            {:ok, _service} -> :ok
+            {:error, _} = error -> raise error
+          end
+        end
+      end)
+    end
 
     :ok
   end
