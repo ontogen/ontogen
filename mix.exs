b8c162744a11dc5ad6ca494d4a3b9bb0de9d7e9d
Marcel Otto <marcelotto@gmx.de>
2024-08-08 12:58:52 +0200

    Setup CI

--- mix.exs
+++ mix.exs
@@ -66,7 +66,9 @@ defmodule Ontogen.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      # This dependency is needed for ExCoveralls when OTP < 25
+      {:castore, "~> 1.0", only: :test}
     ]
   end
 
